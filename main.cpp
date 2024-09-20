0511e5a4d0130333a270474c980db4261b0e89eb
Ali Barzegari <116592901+Ali-Barzegari-d@users.noreply.github.com>
2024-09-21 00:33:42 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
-
-#include<iostream>
-
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-class atm				// class atm 
+class atm				
 {
-private:				// private member varibles
+private:				
 	long int account_No;
 	string name;
 	int PIN;
 	double balance;
 	string mobile_No;
 
-public:				// public member functions
-
+public:				
 	// setData function is setting the Data into the private member variables
 	void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a)
 	{
@@ -25,31 +20,26 @@ public:				// public member functions
 		balance = balance_a;
 		mobile_No = mobile_No_a;
 	}
-
 	//getAccountNo function is returning the user's account no.
 	long int getAccountNo()
 	{
 		return account_No;
 	}
-
 	//getName function is returning the user's Name
 	string getName()
 	{
 		return name;
 	}
-
 	//getPIN function is returning the user's PIN
 	int getPIN()
 	{
 		return PIN;
 	}
-
 	//getBalance is returning the user's Bank Balance
 	double getBalance()
 	{
 		return balance;
 	}
-
 	//getMobileNo is returning the user's Mobile No.
 	string getMobileNo()
 	{
@@ -59,17 +49,14 @@ public:				// public member functions
 	//setMobile function is Updating the user mobile no
 	void setMobile(string mob_prev, string mob_new)
 	{
-		if (mob_prev == mobile_No)		// it will check old Mobile no
+		if (mob_prev == mobile_No)	
 		{
-			mobile_No = mob_new;		// and Update with new, if old matches
-			cout << endl << "Sucessfully Updated Mobile no.";
-						//getch is to hold the screen untill user press any key 																			
+			mobile_No = mob_new;		
+			cout << endl << "Sucessfully Updated Mobile no.";																				
 		}
-
-		else							// Does not update if old mobile no. does not matches
+		else							
 		{
-			cout << endl << "Incorrect !!! Old Mobile no";
-					  
+			cout << endl << "Incorrect !!! Old Mobile no";		  
 		}
 	}
 
@@ -81,25 +68,21 @@ public:				// public member functions
 			balance -= amount_a;		// balance = balance - amount_a
 			cout << endl << "Please Collect Your Cash";
 			cout << endl << "Available Balance :" << balance;
-							
 		}
 
 		else
 		{
-			cout << endl << "Invalid Input or Insufficient Balance";
-							
+			cout << endl << "Invalid Input or Insufficient Balance";						
 		}
 	}
-
 };
 
 int main()
 {
-	int choice = 0, enterPIN;		//enterPIN and enterAccountNo. ---> user authentication
+	int choice = 0, enterPIN;		//enterPIN and enterAccountNo. 
 	long int enterAccountNo;
 
 	system("cls");
-
 	// created User ( object )
 	atm user1;
 	// Set User Details ( into object ) ( Setting Default Data )
@@ -116,10 +99,6 @@ int main()
 
 		cout << endl << "Enter PIN ";		 // asking user to enter PIN
 		cin >> enterPIN;
-
-
-
-		// check whether enter values matches with user details
 		if ((enterAccountNo == user1.getAccountNo()) && (enterPIN == user1.getPIN()))
 		{
 			do
@@ -143,17 +122,12 @@ int main()
 				case 1:							
 					cout << endl << "Your Bank balance is :" << user1.getBalance();
 					// getBalance is ... printing the users	bank balance
-					
 					break;
-
-
 				case 2:								
 					cout << endl << "Enter the amount :";
 					cin >> amount;
-					user1.cashWithDraw(amount);			// calling cashWithdraw function 
-														// passing the withdraw amount 
+					user1.cashWithDraw(amount);			// calling cashWithdraw function 		
 					break;
-
 
 				case 3:								
 					cout << endl << "*** User Details are :- ";
@@ -162,9 +136,7 @@ int main()
 					cout << endl << "-> Balance   :" << user1.getBalance();
 					cout << endl << "-> Mobile No. :" << user1.getMobileNo();
 					// getting and printing user details
-					
 					break;
-
 
 				case 4:								 
 					cout << endl << "Enter Old Mobile No. ";
@@ -175,8 +147,6 @@ int main()
 
 					user1.setMobile(oldMobileNo, newMobileNo);	// now set new mobile no
 					break;
-
-					
 				case 5:
 					exit(0);		// exit application
 
